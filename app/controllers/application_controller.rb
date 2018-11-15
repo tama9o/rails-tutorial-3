a3338efdee8e6967531f75d7e9582cdf79427844
tama9o <tama9o@hotmail.com>
2018-11-15 22:50:30 +0900

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
+# vim: set ts=2 sw=2 expandtab:
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
 end
